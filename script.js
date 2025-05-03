4170fe50962b7d2e1375802224da094ff4573ff5
rubyg-2 <rubygonzalez004@gmail.com>
2025-05-03 12:30:41 -0700

    Update script.js

--- script.js
+++ script.js
@@ -87,25 +87,3 @@ video.addEventListener('ended', function() {
 });
 
 
-
-function toggleDetails(btn) {
-  const details = btn.nextElementSibling;
-  details.classList.toggle('hidden');
-  btn.textContent = details.classList.contains('hidden') ? 'Read More' : 'Hide';
-}
-
-
-let currentIndex = 0;
-
-  function slide(direction) {
-    const slider = document.getElementById('imageSlider');
-    const totalImages = slider.children.length;
-    const containerWidth = slider.parentElement.offsetWidth;
-
-    currentIndex += direction;
-
-    if (currentIndex < 0) currentIndex = totalImages - 1;
-    if (currentIndex >= totalImages) currentIndex = 0;
-
-    slider.style.transform = `translateX(-${currentIndex * containerWidth}px)`;
-  }
